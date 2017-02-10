6c2ca0e39d22198f11e17d761958e6d25fed273f
Shurup228 <yroslav541@gmail.com>
2017-02-11 01:13:41 +0200

    Last edit of manifest(i hope) and message passing.

--- background.js
+++ background.js
@@ -1,1 +1,22 @@
 'use strict'
+
+{
+  const popup = chrome.extension.getViews({ type: 'popup' })[0];
+  const doc = popup.document;
+
+  const dButton = doc.getElementById('download');
+  const cfButton = doc.getElementById('setFolder');
+
+  dButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { clicked: 'downloadButton' }, function (response) {});
+    });
+  });
+
+  cfButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { clicked: 'cfButton' }, function (response) {});
+    });
+
+  });
+}
--- content.js
+++ content.js
@@ -1,1 +1,11 @@
 'use strict'
+
+chrome.runtime.onMessage.addListener(function (mes, sen, resp) {
+  if (mes.clicked === 'cfButton') {
+    // Choose folder stuff
+    console.log('cfButton');
+  } else {
+    // Download stuff
+    console.log('dButton');
+  }
+});
