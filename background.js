ea7edb94355540c2fab6d8c3e7660522ac83fbd0
Shurup228 <yroslav541@gmail.com>
2017-02-11 02:37:36 +0200

    Skeleton complete. Implementing methods.

--- background.js
+++ background.js
@@ -5,18 +5,10 @@
   const doc = popup.document;
 
   const dButton = doc.getElementById('download');
-  const cfButton = doc.getElementById('setFolder');
 
   dButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { clicked: 'downloadButton' }, function (response) {});
     });
-  });
-
-  cfButton.addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { clicked: 'cfButton' }, function (response) {});
-    });
-
   });
 }
--- content.js
+++ content.js
@@ -1,11 +1,4 @@
 'use strict'
 
 chrome.runtime.onMessage.addListener(function (mes, sen, resp) {
-  if (mes.clicked === 'cfButton') {
-    // Choose folder stuff
-    console.log('cfButton');
-  } else {
-    // Download stuff
-    console.log('dButton');
-  }
 });
