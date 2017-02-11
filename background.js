4084d1330e5b2cd5f4cdc4937c9031c8faa3cd49
Shurup228 <yroslav541@gmail.com>
2017-02-11 12:32:45 +0200

    working on scrolling to bottom of audio

--- background.js
+++ background.js
@@ -4,11 +4,23 @@
   const popup = chrome.extension.getViews({ type: 'popup' })[0];
   const doc = popup.document;
 
-  const dButton = doc.getElementById('download');
+  const cont = doc.getElementById('download');
 
-  dButton.addEventListener('click', function () {
+  cont.addEventListener('mouseover', function () {
+    const img = cont.querySelector('img');
+    img.setAttribute('src', 'button_white.png');
+  });
+
+  cont.addEventListener('mouseleave', function () {
+    const img = cont.querySelector('img');
+    img.setAttribute('src', 'button.png');
+  });
+
+  cont.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { clicked: 'downloadButton' }, function (response) {});
+      const t = tabs[0];
+      if (!t.url.startsWith('https://vk.com/audios')) alert('This is not vk[audio] page!');
+      chrome.tabs.sendMessage(t.id, { clicked: 'downloadButton' }, function (response) {});
     });
   });
 }
--- content.js
+++ content.js
@@ -1,4 +1,24 @@
 'use strict'
 
+function scrollBottom() {
+  let getLen = () => {
+    const a = document.querySelectorAll('div.audio_row');
+    return a.length;
+  };
+
+  let audios = document.querySelectorAll('div.audio_row');
+  let lastL = audios.length;
+  let newL = 0;
+  const bot = document.getElementById('ui_audio_load_more');
+
+  while (newL !== lastL) {
+    lastL = getLen();
+    bot.scrollIntoView();
+    newL = getLen();
+  }
+}
+
 chrome.runtime.onMessage.addListener(function (mes, sen, resp) {
+  console.log('lol');
+  scrollBottom();
 });
