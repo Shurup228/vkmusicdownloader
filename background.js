066356c1eecd2e071a0af17cdab77beb4aad6b6c
Shurup228 <yroslav541@gmail.com>
2017-02-12 21:10:19 +0200

    Implemented downloads queue and buffer; working on filesystem;

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 'use strict'
 
 {
+  const QUEUE = [];
+  const BUFFER = [];
+
   const popup = chrome.extension.getViews({ type: 'popup' })[0];
   const doc = popup.document;
 
@@ -20,11 +23,19 @@
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const t = tabs[0];
       if (!t.url.startsWith('https://vk.com/audio')) alert('This is not vk[audio] page!');
-      chrome.tabs.sendMessage(t.id, { clicked: 'downloadButton' }, function (response) {});
+      chrome.tabs.sendMessage(t.id, { clicked: 'downloadButton' });
     });
   });
 
   chrome.runtime.onMessage.addListener(function (mes, sen, resp) {
-    console.log(mes);
+    chrome.downloads.download({ url: mes.url, filename: mes.author + ' - ' + mes.title },
+    (item) => QUEUE.push(item));
   });
+
+  while (true) {
+    console.log(`queue: ${QUEUE}\nbuffer: ${BUFFER}`);
+    BUFFER = BUFFER.filter((el, i, arr) => el.state != 'complete');
+    if (BUFFER.length < 10) BUFFER.push(QUEUE.pop());
+    if ((!QUEUE.length) && (!BUFFER.length)) break;
+  }
 }
